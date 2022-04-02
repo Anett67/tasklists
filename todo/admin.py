49f91ceb144425693b9dee64f39618eb8426fe94
Anett DAVID <davidanett@gmail.com>
2022-04-02 15:16:18 +0200

    Add models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Task, Tasklist
 
-# Register your models here.
+admin.site.register(Tasklist)
+admin.site.register(Task)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tasklist(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    archived = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    priority = models.BooleanField(default=False)
+    tasklist = models.ForeignKey(Tasklist, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
