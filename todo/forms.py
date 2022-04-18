c13d97cd595a13338b3f09db791db198ce4a280b
Anett DAVID <davidanett@gmail.com>
2022-04-18 14:57:25 +0200

    Style update tasklist form

--- todo/forms.py
+++ todo/forms.py
@@ -2,6 +2,11 @@ from django.forms import ModelForm
 from .models import Tasklist, Task, Profil
 
 class TasklistForm(ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(TasklistForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
     class Meta:
         model = Tasklist
         fields = ['title']
