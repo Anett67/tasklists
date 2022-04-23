87dfcc7cf5dd4c78b7bc70557ad9580ae539a751
Anett DAVID <davidanett@gmail.com>
2022-04-23 11:50:39 +0200

    Style task form

--- todo/forms.py
+++ todo/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Tasklist, Task, Profil
+from crispy_forms.helper import FormHelper
 
 class TasklistForm(ModelForm):
     
@@ -12,9 +13,14 @@ class TasklistForm(ModelForm):
         fields = ['title']
 
 class TaskForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
     class Meta:
         model = Task
         fields = ['title', 'description', 'priority']
+        
 
 class ProfileForm(ModelForm):
     class Meta:
--- todowoo/settings.py
+++ todowoo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ LOGIN_URL = '/connexion'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
